09f498fbb49654c4d1cdcdf13e8a7a8716b670ce
NguyenVuKhacDiep <khacdiep20062000@gmail.com>
2022-11-26 21:27:49 +0700

    content5

---
+++ frontend/src/component/content5/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../general/css/grid.css'
+import './css/index.css'
+// import './assets/css/res.css'
+import '../general/fontawesome-free-6.2.0-web/css/all.min.css'
+
+
+export const Content5 = () => {
+  return (
+    <section className='content5'>
+        <h3>NEWSLETTER</h3>
+        <p>Subscribe to our newsletter and get <span className='content__percent'>10%</span> off your first purchase</p>
+        <div className='content5__promote'>
+            <input type="email" className='content5__email' name="" id="" placeholder='Your email address'  required />
+            <button className='content5__btn'>SUBSCRIBE</button>
+        </div>
+    </section>
+
+    )
+}
