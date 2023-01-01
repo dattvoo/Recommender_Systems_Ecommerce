bdafc0eee12af679934dae2986ca6cd1a8e2cd1c
Dat Vo <datvodat288@gmail.com>
2023-01-01 22:28:25 +0700

    fix login form

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,32 +1,30 @@
 import { Button, Form, Input } from "antd";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { FormStyle, WrapForm, Wrapper } from "./style";
+import { FormStyle, Heading, WrapForm, Wrapper } from "./style";
 import "./style.css";
-
-
-
 
 export const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const onFinish = async (values) => {
     try {
-      // setLoading(true);
       const { data } = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/login`,
-        values,
-
+        values
       );
       dispatch({ type: "LOGIN", payload: data });
       Cookies.set("user", JSON.stringify(data));
       navigate("/");
     } catch (error) {
       // setLoading(false);
-      // setError(error.response.data.message);
+      setError(error.response.data.message);
     }
   };
 
@@ -37,6 +35,10 @@ export const Login = () => {
   return (
     <Wrapper className="test">
       <WrapForm>
+        <Heading>
+          <h1>Login Form</h1>
+        </Heading>
+
         <FormStyle
           name="basic"
           labelCol={{ span: 8 }}
@@ -44,16 +46,20 @@ export const Login = () => {
           initialValues={{ remember: true }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
-          autoComplete="off"
         >
           <Form.Item
             label="Username"
             name="username"
+            required
             rules={[{ required: true, message: "Please input your username!" }]}
           >
             <Input />
           </Form.Item>
-
+          {error && (
+            <div className="error_message">
+              <span>{error}</span>
+            </div>
+          )}
           <Form.Item
             label="Password"
             name="password"
@@ -61,9 +67,18 @@ export const Login = () => {
           >
             <Input.Password />
           </Form.Item>
+          <Link href="/register" className="link_register">
+            Do Not Account? Register Now!
+          </Link>
+
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button type="primary" htmlType="submit" className="btn btn-submit">
               LOGIN
+            </Button>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" className="btn btn-login">
+              Login as guest
             </Button>
           </Form.Item>
         </FormStyle>
--- frontend/src/pages/Login/style.ts
+++ frontend/src/pages/Login/style.ts
@@ -9,19 +9,52 @@ export const Wrapper = styled.div`
     rgba(246, 130, 35, 0.9640231092436975) 7%,
     rgba(208, 119, 64, 0.927608543417367) 79%
   );
+  text-align: center;
+  .link_register {
+    display: inline-block;
+    margin-bottom: 16px;
+    /* transform: translateX(-24px); */
+  }
+  .btn {
+    border-radius: 8px;
+  }
+  .btn-login {
+    background-color: #ad7e43;
+  }
+  .btn-submit {
+    background-color: #851010;
+  }
+  .ant-form-item .ant-form-item-explain-error {
+    color: #5c0203;
+  }
+  .error_message {
+    margin-bottom: 8px;
+  }
+  .error_message span {
+    margin-left: 180px;
+    color: #5c0203;
+  }
 `;
 
 export const WrapForm = styled.div`
-  position: fixed;
-  top: 35%;
-  left: 25%;
-  width: 500px;
-  min-height: 500px;
+  width: 650px;
+  /* min-height: 700px; */
+  padding-top: 100px;
+  margin: 0 auto;
+  transform: translateX(-15%);
 `;
 
 export const FormStyle = styled(AntForm)`
-  /* width: 500px; */
-  /* height: 500px; */
-  /* margin: 0 auto; */
-  /* padding-top: 40vh; */
+  /* width: 500px; 
+  height: 500px;
+  margin: 0 auto;
+  padding-top: 40vh; */
+`;
+export const Heading = styled.div`
+  text-align: center;
+  margin-bottom: 40px;
+  h1 {
+    /* color :red; */
+    margin-left: 150px;
+  }
 `;
